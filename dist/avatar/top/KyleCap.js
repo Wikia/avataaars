4efd536f735b21996fd09a82972d119dc7ef4747
xkxd <krzysztof.derek@gmail.com>
2019-12-06 15:12:13 +0100

    Vader, Spidey, Kyle, Pikachu

---
+++ dist/avatar/top/KyleCap.js
@@ -0,0 +1,30 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var KylesCap = /** @class */ (function (_super) {
+    __extends(KylesCap, _super);
+    function KylesCap() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    KylesCap.prototype.render = function () {
+        return (React.createElement("g", { id: 'Top', strokeWidth: '1', fillRule: 'evenodd', transform: 'scale(0.8), translate(35, 25)' },
+            React.createElement("path", { fill: 'rgb(51, 204, 0)', fillOpacity: '1', fillRule: 'evenodd', stroke: 'none', strokeWidth: '1px', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeOpacity: '1', d: 'M 38.331782,84.31125 L 38.331782,23.338763 C 38.331782,23.338763 36.559326,10.931571 49.32101,10.931571 C 62.08269,10.931571 209.90552,10.931571 209.90552,10.931571 C 209.90552,10.931571 223.0217,10.57708 222.66721,25.820202 C 222.31271,41.063323 222.66721,89.27413 222.66721,89.27413 L 38.331782,84.31125 z ', id: 'path6378' }),
+            React.createElement("path", { fill: 'rgb(51, 204, 0)', fillOpacity: '1', fillRule: 'evenodd', stroke: 'none', strokeWidth: '1px', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeOpacity: '1', d: 'M 37.228117,83.8382 C 37.228117,83.8382 -8.260157,131.34432 12.66313,165.05305 C 29.809448,192.67685 62.9332,117.19823 56.55236,97.70121 C 50.17152,78.20419 36.346365,84.58504 37.228117,83.8382 z ', id: 'path6374' }),
+            React.createElement("path", { fill: 'rgb(51, 204, 0)', fillOpacity: '1', fillRule: 'evenodd', stroke: 'none', strokeWidth: '1px', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeOpacity: '1', d: 'M 223.15575,85.93302 C 223.15575,85.93302 267.35906,133.43914 247.02682,167.14787 C 230.36485,194.77167 198.17679,119.29305 204.37738,99.79603 C 210.57797,80.29901 224.01259,86.67986 223.15575,85.93302 z ', id: 'path6376' }),
+            React.createElement("path", { fill: 'none', fillRule: 'evenodd', stroke: 'black', strokeWidth: '2', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeOpacity: '1', strokeMiterlimit: '4', strokeDasharray: 'none', d: 'M 221.67798,85.393092 C 221.67798,85.393092 127.27922,43.673792 37.476659,83.625325', id: 'path14403' }),
+            React.createElement("path", { fill: 'rgb(2, 168, 2)', fillOpacity: '1', fillRule: 'evenodd', stroke: 'none', strokeWidth: '1px', strokeLinecap: 'butt', strokeLinejoin: 'miter', strokeOpacity: '1', d: 'M 47.761273,45.732095 L 46.758621,98.37135 C 46.758621,98.37135 45.254642,100.37666 51.27056,99.87533 C 57.28647,99.37401 97.47416,91.97638 134.74138,91.85411 C 161.87597,91.76509 207.18303,95.86472 208.687,99.374 C 210.19098,102.88328 214.20159,88.34483 213.19894,81.82759 C 212.19629,75.31034 212.19629,46.233422 212.19629,46.233422 C 212.19629,46.233422 49.26525,45.732095 47.761273,45.732095 z ', id: 'path1899' })));
+    };
+    KylesCap.optionValue = 'Kyle\'s cap';
+    return KylesCap;
+}(React.Component));
+exports.default = KylesCap;
---
+++ dist/avatar/top/Vader.js
@@ -0,0 +1,72 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var eyes_1 = require("../face/eyes");
+var mouth_1 = require("../face/mouth");
+var Vader = /** @class */ (function (_super) {
+    __extends(Vader, _super);
+    function Vader() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Vader.prototype.render = function () {
+        return (React.createElement("g", null,
+            React.createElement("g", { transform: 'scale(0.48) translate(-45, -90)' },
+                React.createElement("defs", null,
+                    React.createElement("clipPath", { id: 'clip-Artboard_1' },
+                        React.createElement("rect", { width: '645', height: '673' }))),
+                React.createElement("g", { id: 'Artboard_1', "data-name": 'Artboard – 1', clipPath: 'url(#clip-Artboard_1)' },
+                    React.createElement("path", { id: 'inside', d: 'M444.637,330.727c-18.936,5.8-61.272-25.49-124.573,33.686.079-.154-72.1,151.2-27.061,165.658.463.449,71.919,22.613,151.634,17.717-.108.04-9,.445,3.934,0,29.795,1.282,118.494,3.048,139.537-17.717,26.043-25.7,8.171-62.725-22.144-165.658C566.059,364.777,541.862,312.393,444.637,330.727Z', transform: 'translate(-122 -71)', fill: '#008c8f', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("g", { id: 'Polygon_1', "data-name": 'Polygon 1', transform: 'translate(378 539) rotate(180)', fill: '#002a32', strokeLinecap: 'round' },
+                        React.createElement("path", { d: 'M 114.5476913452148 89.5 L 6.452312469482422 89.5 L 60.49996948242188 6.41842794418335 L 114.5476913452148 89.5 Z', stroke: 'none' }),
+                        React.createElement("path", { d: 'M 60.49998474121094 12.83684539794922 L 12.90461730957031 86 L 108.0953903198242 86 L 60.49998474121094 12.83684539794922 M 60.49998092651367 0 L 121 93 L 7.62939453125e-06 93 L 60.49998092651367 0 Z', stroke: 'none', fill: '#0d0d0d' })),
+                    React.createElement("path", { id: 'Path_1', "data-name": 'Path 1', d: 'M350.418,467.816,372.7,503.77', transform: 'translate(-124 -69)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("g", { id: 'Ellipse_1', "data-name": 'Ellipse 1', transform: 'translate(242 430)', fill: '#002a32', stroke: '#0d0d0d', strokeWidth: '7' },
+                        React.createElement("circle", { cx: '16.5', cy: '16.5', r: '16.5', stroke: 'none' }),
+                        React.createElement("circle", { cx: '16.5', cy: '16.5', r: '13', fill: 'none' })),
+                    React.createElement("g", { id: 'Ellipse_2', "data-name": 'Ellipse 2', transform: 'translate(361 430)', fill: '#002a32', stroke: '#0d0d0d', strokeWidth: '7' },
+                        React.createElement("circle", { cx: '16.5', cy: '16.5', r: '16.5', stroke: 'none' }),
+                        React.createElement("circle", { cx: '16.5', cy: '16.5', r: '13', fill: 'none' })),
+                    React.createElement("path", { id: 'Path_4', "data-name": 'Path 4', d: 'M372.7,467.816,350.418,503.77', transform: 'translate(39 -69)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_5', "data-name": 'Path 5', d: 'M428.727,433.531v24.363h32.938V433.531l-15.941-14.5Z', transform: 'translate(-124 -69)', fill: '#002a32', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("g", { id: 'Ellipse_3', "data-name": 'Ellipse 3', transform: 'translate(226 294)', fill: 'none', stroke: '#0d0d0d', strokeWidth: '7' },
+                        React.createElement("ellipse", { cx: '39.5', cy: '28', rx: '39.5', ry: '28', stroke: 'none' }),
+                        React.createElement("ellipse", { cx: '39.5', cy: '28', rx: '43', ry: '31.5', fill: 'none' })),
+                    React.createElement("g", { id: 'Ellipse_4', "data-name": 'Ellipse 4', transform: 'translate(342 294)', fill: 'none', stroke: '#0d0d0d', strokeWidth: '7' },
+                        React.createElement("ellipse", { cx: '39.5', cy: '28', rx: '39.5', ry: '28', stroke: 'none' }),
+                        React.createElement("ellipse", { cx: '39.5', cy: '28', rx: '43', ry: '31.5', fill: 'none' })),
+                    React.createElement("path", { id: 'Path_11', "data-name": 'Path 11', d: 'M328.938,419l-25.711,84.16', transform: 'translate(-124 -69)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_12', "data-name": 'Path 12', d: 'M321.743,509.34s-25.575-2.07-38.489-10.707', transform: 'translate(-124 -69)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_13', "data-name": 'Path 13', d: 'M283.254,509.34s25.575-2.07,38.489-10.707', transform: 'translate(154 -69)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_14', "data-name": 'Path 14', d: 'M303.227,419l25.711,84.16', transform: 'translate(139 -70)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_15', "data-name": 'Path 15', d: 'M389.5,499l37.508-48.02', transform: 'translate(-121 -65)', fill: 'none', stroke: '#0d0d0d', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_16', "data-name": 'Path 16', d: 'M427.008,499,389.5,450.98', transform: 'translate(-54 -65)', fill: 'none', stroke: '#0d0d0d', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_18', "data-name": 'Path 18', d: 'M414.961,481.165V522.6', transform: 'translate(-124 -73.962)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_19', "data-name": 'Path 19', d: 'M414.961,481.165V522.6', transform: 'translate(-64 -72.962)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_20', "data-name": 'Path 20', d: 'M415.421,481.165v56.557', transform: 'translate(-105.034 -89.085)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_21', "data-name": 'Path 21', d: 'M415.421,481.165v56.557', transform: 'translate(-85.034 -89.085)', fill: 'none', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("path", { id: 'Path_22', "data-name": 'Path 22', d: 'M321.743,363c.114,2.441-17.609-139.873,121.985-139.232S568.553,360.965,567.544,363,533.7,305.458,443.728,332.669c-1.02-1-44-10.8-70.453,0C343.389,339.326,321.673,361.5,321.743,363Z', transform: 'translate(-124 -69)', fill: '#002a32', stroke: '#0d0d0d', strokeLinecap: 'round', strokeWidth: '7' }),
+                    React.createElement("g", { id: 'Rectangle_1', "data-name": 'Rectangle 1', transform: 'translate(305 141)', fill: '#002a32', stroke: '#0d0d0d', strokeWidth: '7' },
+                        React.createElement("rect", { width: '33', height: '134', stroke: 'none' }),
+                        React.createElement("rect", { x: '-3.5', y: '-3.5', width: '40', height: '141', fill: 'none' })))),
+            React.createElement("g", { id: 'eyz', fill: "white", transform: "translate(77, 80)" },
+                React.createElement(eyes_1.default, null),
+                React.createElement(eyes_1.default, null),
+                React.createElement(eyes_1.default, null),
+                React.createElement(eyes_1.default, null),
+                React.createElement(eyes_1.default, null),
+                React.createElement(mouth_1.default, null))));
+    };
+    Vader.optionValue = 'Vader';
+    return Vader;
+}(React.Component));
+exports.default = Vader;
